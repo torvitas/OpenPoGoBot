ce956e80527bb6642ef581d515371119df95243f
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-08-19 14:12:27 +0200

    makes transfer_pokemon respect keep: false

--- plugins/transfer_pokemon/__init__.py
+++ plugins/transfer_pokemon/__init__.py
@@ -118,7 +118,7 @@ class TransferPokemon(Plugin):
         for pokemon in transfer_list:
             species_num = pokemon.pokemon_id
             species_name = bot.pokemon_list[species_num - 1]["Name"]
-            if species_name not in always_keep_list or always_keep_list[species_name] is False:
+            if species_name not in always_keep_list or always_keep_list[species_name]["keep"] is False:
                 new_transfer_list.append(pokemon)
             else:
                 excluded_species.add(species_name)
